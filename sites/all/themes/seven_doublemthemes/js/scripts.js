252efc2171d763950971fe8d1b185c155c41b162
Rich <projects@tragicmedia.com>
2012-11-13 18:33:23 -0500

    latest theming updates and javascript

--- sites/all/themes/seven_doublemthemes/js/scripts.js
+++ sites/all/themes/seven_doublemthemes/js/scripts.js
@@ -4,22 +4,37 @@
 *    Created on: 11/8/12
 ***********************************/
 var $ = jQuery.noConflict();
-$(document).ready(function() {
-if($('#block-views-earlyiq-slideshow-block-block').length) {
-	var constants = {
-        container:'#block-views-earlyiq-slideshow-block-block .view-content .item-list',
-        itemClass:'.views-row',
-        pageDots:true,
-        interval:true,
-        intervalSpeed: 8500,
-        slideNumber:1,
-        numberShown:1,
-        speed:500,
-        initFunc:function(){},
-      	slideInitFunc:function(){},
-  		slideEndFunc:function(){}
+    
+    $(document).ready(function() {
+    
+    if($('#block-views-earlyiq-slideshow-block-block').length) {
+    	var constants = {
+            container:'#block-views-earlyiq-slideshow-block-block .view-content .item-list',
+            itemClass:'.views-row',
+            pageDots:true,
+            interval:true,
+            intervalSpeed: 8500,
+            slideNumber:1,
+            numberShown:1,
+            speed:500,
+            initFunc:function(){},
+          	slideInitFunc:function(){},
+      		slideEndFunc:function(){}
+        }
+        var scroll = new dScroll(constants);
     }
-    var scroll = new dScroll(constants);
-}
+
+    if($('#block-system-main-menu').length) {
+        predef('#block-system-main-menu li a');
+    }
 
 });
+
+function predef(menu) {
+    $(menu).click(function(e){
+        // except for 'about'
+        if(!($(this).hasClass('menu-minipanel-1231'))) {
+            e.preventDefault();
+        }
+    });
+}
