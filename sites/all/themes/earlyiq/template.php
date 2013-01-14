883884bdf945ed693bf257c9b269a5e9520dd68e
Troy Dohner <troy@husointeractive.com>
2013-01-14 15:24:07 -0800

    removing steps from verification/#nid#/complete page

--- sites/all/themes/earlyiq/template.php
+++ sites/all/themes/earlyiq/template.php
@@ -127,7 +127,7 @@ function earlyiq_preprocess_page(&$variables, $hook) {
   } else if (isset($variables['node']) && $variables['node']->type == 'data_person') {
     $variables['show_steps'] = '<div id="show-steps"><div class="show-steps-inner"><span id="step1" class="activeStep">Step 1</span><span id="step2" class="activeStep">Step 2</span><span id="step3">Step 3</span></div></div>';
 
-  } else if (arg(0) == "verification") {
+  } else if (arg(0) == "verification" && arg(2) != "complete") {
     $variables['show_steps'] = '<div id="show-steps"><div class="show-steps-inner"><span id="step1" class="activeStep">Step 1</span><span id="step2" class="activeStep">Step 2</span><span id="step3" class="activeStep">Step 3</span></div></div>';
   }
 }
